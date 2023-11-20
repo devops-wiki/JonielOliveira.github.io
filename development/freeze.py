2e86606d3461bcfac325ee2f4d78fe27ccb1bd87
Joniel R. de Oliveira <joniel.oliveira@fatec.sp.gov.br>
2023-11-20 19:05:37 -0300

    Alteração no build
    Alteração da pasta onde é construído as página do Frozen.

--- development/freeze.py
+++ development/freeze.py
@@ -5,7 +5,7 @@ from os import path
 app.config.update(
     FREEZER_IGNORE_MIMETYPE_WARNINGS = True,
     FREEZER_RELATIVE_URLS = False,
-    FREEZER_DESTINATION = path.dirname(path.dirname(__file__)),
+    # FREEZER_DESTINATION = path.dirname(path.dirname(__file__)),
     FREEZER_REMOVE_EXTRA_FILES = False
 )
 
